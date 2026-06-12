88716acc0b2827fc4ad90dd44516b96c2aeb35b1
pained-chicken <endusj01@kakao.com>
2026-06-12 16:48:36 +0900

    기본 레이아웃

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const expressLayout = require("express-ejs-layouts");
 
 const app = express();
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
